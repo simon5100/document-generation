de87d5ff98836b5af8f8f5b5f663a0c16ab771be
simon5100 <simon51005100@gmail.com>
2025-09-02 21:23:34 +0700

    Initial commit

---
+++ src/main/java/com/simon/documentgeneration/DocumentGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.simon.documentgeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentGenerationApplication.class, args);
+	}
+
+}
